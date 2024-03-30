5d6ff2d713cb53e21b0e3f5c9b990a77f88aea0a
Huy <huynguyen030704@gmail.com>
2024-03-30 11:40:01 +0700

    server 30/3 fix connect DB visson

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      "mongodb+srv://huynguyen030704:Huynguyen2004@cluster0.xdzqfz6.mongodb.net/CN_LEAN"
+    );
+    // await mongoose.connect(process.env.MONGO_URL);
     console.log("Connect MongoDB");
   } catch (error) {
     console.log("Connect Failed " + error.messenger);
