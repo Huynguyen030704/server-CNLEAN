2508551009d505a9a3a53a7c85ea6c95a8b07d18
Huy <huynguyen030704@gmail.com>
2024-03-30 16:19:21 +0700

    server 30/3 fix connect DB

--- connectMongo.js
+++ connectMongo.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    mongoose.connect(
-      "mongodb+srv://huynguyen030704:Huynguyen2004@cluster0.xdzqfz6.mongodb.net/CN_LEAN"
-    );
-    // await mongoose.connect(process.env.MONGO_URL);
+    // await mongoose.connect(
+    //   "mongodb+srv://huynguyen030704:Huynguyen2004@cluster0.xdzqfz6.mongodb.net/CN_LEAN"
+    // );
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connect MongoDB");
   } catch (error) {
     console.log("Connect Failed " + error.messenger);
