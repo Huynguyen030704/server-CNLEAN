e58d100582ffc8d0e099ce94fbdde7afd2f036c2
Huy <huynguyen030704@gmail.com>
2024-03-30 16:27:20 +0700

    server 30/3 fix connect DB

--- connectMongo.js
+++ connectMongo.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://huynguyen030704:Huynguyen2004@cluster0.xdzqfz6.mongodb.net/CN_LEAN"
-    );
-    // await mongoose.connect(process.env.MONGO_URL, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
+    // await mongoose.connect(
+    //   "mongodb+srv://huynguyen030704:Huynguyen2004@cluster0.xdzqfz6.mongodb.net/CN_LEAN"
+    // );
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log("Connect MongoDB");
   } catch (error) {
     console.log("Connect Failed " + error.messenger);
